74e7da0ac4729c9aeb67fd77466846256c663371
MarisMihailovs <marismihailovs@gmail.com>
2022-07-27 02:15:47 +0300

    add timer logic

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+const clock = document.getElementById('clock');
+const btnMin1 = document.getElementById('1min');
+const btnMin3 = document.getElementById('3min');
+const btnMin5 = document.getElementById('5min');
+const btnMin10 = document.getElementById('10min');
+const btnMin15 = document.getElementById('15min');
+const btnMin30 = document.getElementById('30min');
+const btnMin45 = document.getElementById('45min');
+const btnMin60 = document.getElementById('60min');
+const btnStart = document.getElementsByClassName('start');
+const btnReset = document.getElementsByClassName('reset');
+
+
 
 const elem = document.documentElement;
-
+const time = { minutes: 0, seconds: 0 };
+let clockStateSeconds;
+let clockstateMinutes;
 
 /* View in fullscreen */
 function openFullscreen() {
@@ -24,11 +39,61 @@ function closeFullscreen() {
     }
 }
 
-function runTimer() {
+function setTimerClock(timerValue) {
 
-
+    time.minutes = timerValue;
+    time.seconds = 60;
+    if (time.minutes > 5) {
+        clock.textContent = time.minutes + ':00';
+    } else {
+        clock.textContent = '0' + time.minutes + ':00';
+    }
 }
 
 
+function deductSecond() {
+    time.seconds--;
+    clock.textContent = time.minutes + ":" + time.seconds;
+}
 
+function deductMinute() {
+    time.minutes--;
+    clock.textContent = time.minutes + ":" + time.seconds;
+}
 
+function changeTimerSeconds() {
+    deductSecond();
+}
+
+function changeTimerMinutes() {
+    deductMinute();
+}
+
+function runTimer() {
+    console.log(time.minutes, time.seconds)
+    if (!clockStateSeconds && !clockstateMinutes) {
+        time.seconds--;
+        clockStateSeconds = setInterval(changeTimerSeconds, 1000);
+        time.minutes--;
+        clockstateMinutes = setInterval(changeTimerMinutes, 60000);
+    }
+}
+
+function stopTimer() {
+    clearInterval(clockStateSeconds);
+    clearInterval(clockstateMinutes);
+    clockStateSeconds = null;
+    clockstateMinutes = null;
+}
+
+btnMin1.addEventListener('click', (evt) => setTimerClock(1));
+btnMin3.addEventListener('click', (evt) => setTimerClock(3));
+btnMin5.addEventListener('click', (evt) => setTimerClock(5));
+btnMin10.addEventListener('click', (evt) => setTimerClock(10));
+btnMin15.addEventListener('click', (evt) => setTimerClock(15));
+btnMin30.addEventListener('click', (evt) => setTimerClock(30));
+btnMin45.addEventListener('click', (evt) => setTimerClock(45));
+btnMin60.addEventListener('click', (evt) => setTimerClock(60));
+
+btnStart[0].addEventListener('click', runTimer);
+btnReset[0].addEventListener('click', stopTimer);
