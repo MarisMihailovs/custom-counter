09e24e606e116d169ebfe1303b8d8e317d35a3db
MarisMihailovs <marismihailovs@gmail.com>
2022-08-02 16:42:02 +0300

    reading event srcelement values from form

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // set date input minimum with todays date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// title for eventlistener
+
+// take values from from input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[0].value;
+    console.log(e);
+}
+
+countdownForm.addEventListener('submit', updateCountdown);
 
